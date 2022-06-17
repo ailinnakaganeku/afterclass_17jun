e37f539d0de711e4516935794adbd38c9d2a2214
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2022-06-17 20:19:13 -0300

    adding toggle

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import CartWidget from "../CartWidget";
 import "./styles.css";
 
 const menuItems = [
   {
-    id: 1,
+    id: "1",
     label: "Techno",
   },
   {
-    id: 2,
+    id: "2",
     label: "Clothes",
   },
   {
-    id: 3,
+    id: "3",
     label: "Decor",
   },
 ];
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="nav">
-      <h1 className="nav-logo">EightShop</h1>
-      <div className="nav-items">
+      <span className="nav-logo">EightShop</span>
+      <div className={`nav-items ${isOpen ? "open" : ""}`.trim()}>
         {menuItems.map((item) => (
           <a href="/" className="nav-item" key={item.id}>
             {item.label}
           </a>
         ))}
       </div>
-      <CartWidget />
+      <div className="cart">
+        <CartWidget />
+      </div>
+
+      <div className="nav-mobile">
+        <div className="cart-mobile">
+          <CartWidget />
+        </div>
+        <div
+          className={`nav-toggle ${isOpen ? "open" : ""}`.trim()}
+          onClick={() => setIsOpen((isOpen) => !isOpen)}
+        >
+          <div className="bar"></div>
+        </div>
+      </div>
     </div>
   );
 };
